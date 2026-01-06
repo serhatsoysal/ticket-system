7af53d62a03d744ed00b40a8e068898dc4b03ee4
serhatsoysal <serhatsoysalx@icloud.com>
2026-01-06 23:09:07 +0400

    fix: Remove Spring context loading from API Gateway test

--- api-gateway/src/test/java/com/heditra/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/heditra/apigateway/ApiGatewayApplicationTests.java
@@ -1,9 +1,7 @@
 package com.heditra.apigateway;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class ApiGatewayApplicationTests {
 
     @Test
